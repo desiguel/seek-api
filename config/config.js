d2ef8294cb6e74bfdfed2f73ad933b50d7a860d6
Unknown <desiguel@gmail.com>
2016-12-06 09:35:12 +1000

    Updated secrets file.

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 // config / database.js
+var secrets = require('./secrets.js');
 
 module.exports = {
 
-  'secret': 'littleredinteresting',
-  'database': 'mongodb://test:largesway@ds159767.mlab.com:59767/seek-api'
+  'secret': secrets.secret,
+  'database': 'mongodb://' + secrets.username + ':' + secrets.password + '@ds159767.mlab.com:59767/seek-api'
 
 };
---
+++ config/secrets.js
@@ -0,0 +1,9 @@
+//config/secrets.js
+
+module.exports = {
+
+  'secret': 'largeblueglass',
+  'username': 'user2',
+  'password': 'redtapefrench'
+
+};
